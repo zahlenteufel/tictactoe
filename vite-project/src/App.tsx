683271dcea206ea93c4232376e3061f23d1bd9f1
Gaston Bengolea Monzon <gastonbengolea@gmail.com>
2025-02-03 23:46:25 +0100

    Connect to backend.

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function Board() {
+const pollInterval = 1000;
+
+function Board(model: any) {
   return (
     <div className="board">
       {Array.from({ length: 9 }, (_, key) => (
@@ -13,12 +15,40 @@ function Board() {
 
 function App() {
   const [player, setPlayer] = useState("X")
+  const [data, setData] = useState(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch("/api/board").then(response => {
+        response.json().then(d => {
+          if (response.ok) {
+            setData(d);
+            console.log(d);
+            setError(null);
+          } else {
+            setData(null);
+            setError(d);
+          }
+        }
+        )
+      }).catch(err => {
+        setData(null);
+        setError(err + '');
+      })
+    };
+    const intervalId = setInterval(fetchData, pollInterval);
+    return () => clearInterval(intervalId);  // Cleanup interval on component unmount.
+  }, []);
 
   return (
     <>
       <h1>TicTacToe</h1>
       <div className="card">
-        <Board></Board>
+        {
+          error && <div className="error">Error!</div>
+        }
+        <Board model={data}></Board>
         <button onClick={() => setPlayer((p) => p == "X" ? "O" : "X")}>
           Player is {player}
         </button>
