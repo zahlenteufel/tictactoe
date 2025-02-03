83d18e787f1b2479a6d1cb05e64fda180434dab7
Gaston Bengolea Monzon <gastonbengolea@gmail.com>
2025-02-03 22:45:23 +0100

    Some refactor and use button to select player.

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,32 +4,23 @@ import './App.css'
 function Board() {
   return (
     <div className="board">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
-      <div>6</div>
-      <div>7</div>
-      <div>8</div>
-      <div>9</div>
-    </div >
-  )
+      {Array.from({ length: 9 }, (_, key) => (
+        <div key={key}>{key}</div>
+      ))}
+    </div>
+  );
 }
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [player, setPlayer] = useState("X")
 
   return (
     <>
       <h1>TicTacToe</h1>
       <div className="card">
-        <div>
-          <Board></Board>
-        </div>
-
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <Board></Board>
+        <button onClick={() => setPlayer((p) => p == "X" ? "O" : "X")}>
+          Player is {player}
         </button>
       </div>
     </>
