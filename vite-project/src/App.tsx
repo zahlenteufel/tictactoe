5676b4b274128dacc18a9f3215b87b2b6852a360
Gaston Bengolea Monzon <gastonbengolea@gmail.com>
2025-02-04 13:28:01 +0100

    Rendering fetched board.

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,60 +1,76 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 const pollInterval = 1000;
 
-function Board(model: any) {
+interface Model {
+  board: any;
+  is_finished: boolean;
+}
+
+interface BoardProps {
+  model: Model | null;
+}
+
+function Board({ model }: BoardProps) {
+  if (model == null || model == undefined) return <div>Loading...</div>;
+  console.log("update model", model);
   return (
     <div className="board">
-      {Array.from({ length: 9 }, (_, key) => (
-        <div key={key}>{key}</div>
-      ))}
+      {Array.from({ length: 9 }, (_, key) => {
+        const row = ~~(key / 3);
+        const column = ~~(key % 3);
+        return <div key={key}>{model.board[row][column]}</div>;
+      })}
     </div>
   );
 }
 
 function App() {
-  const [player, setPlayer] = useState("X")
-  const [data, setData] = useState(null);
+  const [player, setPlayer] = useState("X");
+  const [data, setData] = useState<Model | null>(null);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      fetch("/api/board").then(response => {
-        response.json().then(d => {
-          if (response.ok) {
-            setData(d);
-            console.log(d);
-            setError(null);
-          } else {
-            setData(null);
-            setError(d);
-          }
-        }
-        )
-      }).catch(err => {
-        setData(null);
-        setError(err + '');
-      })
+      fetch("/api/board")
+        .then((response) => {
+          response.json().then((d) => {
+            if (response.ok) {
+              if (JSON.stringify(d) != JSON.stringify(data)) {
+                setData(d);
+                console.log("Set data: ", d);
+              }
+              setError(null);
+            } else {
+              setData(null);
+              console.log("Set data: ERROR");
+              setError(d);
+            }
+          });
+        })
+        .catch((err) => {
+          setData(null);
+          console.log("there was ERROR");
+          setError(err + "");
+        });
     };
     const intervalId = setInterval(fetchData, pollInterval);
-    return () => clearInterval(intervalId);  // Cleanup interval on component unmount.
-  }, []);
+    return () => clearInterval(intervalId); // Cleanup interval on component unmount.
+  }, [data]);
 
   return (
     <>
       <h1>TicTacToe</h1>
       <div className="card">
-        {
-          error && <div className="error">Error!</div>
-        }
+        {error && <div className="error">Error!</div>}
         <Board model={data}></Board>
-        <button onClick={() => setPlayer((p) => p == "X" ? "O" : "X")}>
+        <button onClick={() => setPlayer((p) => (p == "X" ? "O" : "X"))}>
           Player is {player}
         </button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
