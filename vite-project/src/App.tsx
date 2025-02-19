bc241712c95aa174cb10df41906e678e61bb74d7
Gaston Bengolea Monzon <gastonbengolea@gmail.com>
2025-02-19 09:47:41 +0100

    Refactor frontend.

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,86 +4,15 @@ import _ from "lodash";
 import WaitingRoom from "./WaitingRoom";
 import { pollInterval, apiUrl } from "./constants";
 import Game from "./Game";
+import { Model, Board } from "./Board";
 
-interface Model {
-  board: string[][];
-  is_finished: boolean;
-}
-
-interface BoardProps {
-  model: Model | null;
-  game_id: string;
-  username: string;
-  setError: (error: string | null) => void;
-}
-
-const makeMove = async (
-  row: number,
-  column: number,
-  username: string,
-  game_id: string,
-  setError: (error: string | null) => void
-) => {
-  const rawResponse = await fetch(`${apiUrl}/game/${game_id}/board`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      row: row + 1,
-      column: column + 1,
-      username: username,
-    }),
-  });
-  if (rawResponse.ok) {
-    setError(null);
-    return;
-  }
-  const err = JSON.parse(await rawResponse.text())["error"] as string;
-  console.error(err);
-  setError(err);
-  return;
-};
-
-function Board({ model, setError, username, game_id }: BoardProps) {
-  if (model == null || model == undefined) return <div>Loading...</div>;
-  console.log("update model", model);
-  return (
-    <div>
-      <div className="board">
-        {Array.from({ length: 9 }, (_, key) => {
-          const row = ~~(key / 3);
-          const column = ~~(key % 3);
-          return (
-            <div
-              key={key}
-              onClick={async () => {
-                await makeMove(row, column, username, game_id, setError);
-              }}
-            >
-              {model.board[row][column]}
-            </div>
-          );
-        })}
-      </div>
-      <div> Finished: {model.is_finished ? "true" : "false"}</div>
-    </div>
-  );
-}
-
-interface GameInCourseProps {
-  game_id: string;
-  username: string;
-}
-
-function GameInCourse({ game_id, username }: GameInCourseProps) {
+function GameInCourse({ id, username }: Game) {
   const [data, setData] = useState<Model | null>(null);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(`${apiUrl}/game/${game_id}`);
+      const response = await fetch(`${apiUrl}/game/${id}`);
       if (!response.ok) {
         const error = await response.text();
         console.log(error);
@@ -98,7 +27,7 @@ function GameInCourse({ game_id, username }: GameInCourseProps) {
     };
     const intervalId = setInterval(fetchData, pollInterval);
     return () => clearInterval(intervalId); // Cleanup interval on component unmount.
-  }, [data, error, game_id, setData, setError]);
+  }, [data, error, id, setData, setError]);
 
   return (
     <>
@@ -108,7 +37,7 @@ function GameInCourse({ game_id, username }: GameInCourseProps) {
         <Board
           model={data}
           setError={setError}
-          game_id={game_id}
+          game_id={id}
           username={username}
         ></Board>
       </div>
@@ -122,7 +51,7 @@ function App() {
   if (game === null) {
     return <WaitingRoom initGame={setGame} />;
   } else {
-    return <GameInCourse game_id={game.id} username={game.username} />;
+    return <GameInCourse id={game.id} username={game.username} />;
   }
 }
 
