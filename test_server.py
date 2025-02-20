ccc820b8c23880711ee4776271181a7ed482d918
Gaston Bengolea Monzon <gastonbengolea@gmail.com>
2025-02-20 15:51:03 +0100

    Add tests for /match

--- test_server.py
+++ test_server.py
@@ -1,23 +1,52 @@
+import datetime
 import pytest
 from server import app
+import freezegun
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def client():
     with app.test_client() as client:
         yield client
+    from server import matched, last_update
+
+    matched.clear()
+    last_update.clear()
 
 
-def test_match_endpoint(client):
+def test_match_with_missing_id_fails(client):
     response = client.get("/match")
+
+    assert response.status_code == 400
+
+
+def test_match_success(client):
+    client.get("/match?id=user1")
+    client.get("/match?id=user2")
+
+    response = client.get("/match?id=user1")
 
     assert response.status_code == 200
     assert response.headers["Content-Type"] == "application/json"
     data = response.json
     assert "game_id" in data
+    assert data["game_id"] is not None
 
 
-def test_match_endpoint_with_missing_id(client):
-    response = client.get("/match")
+def test_cant_match_expired_user(client):
+    client.get("/match?id=user1")
 
-    assert response.status_code == 400
+    with freezegun.freeze_time(
+        datetime.datetime.now() + datetime.timedelta(seconds=11)
+    ) as frozen_time:
+        response = client.get("/match?id=user2").json
+
+        assert response["game_id"] is None
+
+
+def test_user_cannot_match_themselves(client):
+    client.get("/match?id=user1")
+
+    response = client.get("/match?id=user1").json
+
+    assert response["game_id"] is None
